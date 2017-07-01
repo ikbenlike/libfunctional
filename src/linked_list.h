7bfe17e2a243c108aaae2d0b7981ee3ad1dc13b9
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-07-01 20:22:25 +0200

    adds inclusion guards to linked_list.h

--- src/linked_list.h
+++ src/linked_list.h
@@ -1,8 +1,11 @@
-typedef struct linked_list_node_t {
-    struct linked_list_node_t *next;
-    void *value;
-} linked_list_node_t;
+#ifndef __LINKED_LIST_H__
+    #define __LINKED_LIST_H__
+    typedef struct linked_list_node_t {
+        struct linked_list_node_t *next;
+        void *value;
+    } linked_list_node_t;
 
-linked_list_node_t *linked_list_construct(size_t len);
-size_t linked_list_len(linked_list_node_t *node);
-int linked_list_free(linked_list_node_t *node);
+    linked_list_node_t *linked_list_construct(size_t len);
+    size_t linked_list_len(linked_list_node_t *node);
+    int linked_list_free(linked_list_node_t *node);
+#endif
